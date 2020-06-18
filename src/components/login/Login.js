84a8711dcc6f0898aad1582646da37510d99f324
Sydney <sydney@artificial.studio>
2020-06-19 00:26:20 +0200

    ui setup

---
+++ src/components/login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import LoggedOutNavigation from '../navigation/LoggedOutNavigation'
+import TextInput from '../shared/TextInput';
+
+export default () => {
+    return (
+        <div className="login">
+            <LoggedOutNavigation />
+            <div className="login-form">
+                <div>
+                    <h1 className="cl-white text-center">Welcome</h1>
+                    <h3 className="cl-white text-center">Login to get started</h3>
+                    <TextInput type="text" className="form-group" name="username" label="Username" />
+                    <TextInput type="password" className="form-group" name="password" label="Password" />
+                    <div className="my-3 d-flex"><a className="cl-white ml-auto" href="#">Forgot Password?</a></div>
+                    <a href="/dashboard" className="btn btn-primary mx-auto">Login</a>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/navigation/LoggedOutNavigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <nav>
+            <img className="mx-auto" src="/images/logo.svg" alt=""/>
+        </nav>
+    )
+}
